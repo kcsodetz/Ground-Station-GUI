a1aeb666006992823d04ad2170b0438ff1b42b42
Ken Sodetz <kensodetz@gmail.com>
2017-10-11 19:36:17 -0400

    add yes or no dialog

--- MainWindow.py
+++ MainWindow.py
@@ -8,20 +8,20 @@ top.geometry("500x500")
 
 # Function to show abort message box
 def abortMessageCallBack():
-    msg = messagebox.showwarning("Abort Mission?", "Do you really want to abort the mission?")
+    msg = messagebox.askyesno("Abort Mission?", "Do you really want to abort the mission?")
 
 
 # Function to show verify message box
 def verifyMessageCallBack():
-    msg = messagebox.showwarning("Verify Launch", "Do you want to verify for launch?")
+    msg = messagebox.askyesno("Verify Launch", "Do you want to verify for launch?")
 
 
 # Abort Mission Button
 abortButton = Button(top, text="ABORT MISSION", bg="red", command=abortMessageCallBack)
-abortButton.place(x=50, y=100)
+abortButton.place(x=50, y=350)
 
 # Verify Launch Button
 verifyButton = Button(top, text="Verify Launch", bg="green", command=verifyMessageCallBack)
-verifyButton.place(x=50, y=150)
+verifyButton.place(x=50, y=400)
 
 top.mainloop()
