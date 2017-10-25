75166a9a2a58ad7d31a2a98f28e4dd634dc4aadf
Ken Sodetz <kensodetz@gmail.com>
2017-10-25 18:13:16 -0400

    add labels and clean up placement

--- MainWindow.py
+++ MainWindow.py
@@ -5,6 +5,9 @@ from tkinter import messagebox
 # Set window size
 top = Tk()
 top.geometry("500x500")
+top.title("Rocket GUI")
+frame = Frame(top, bg="#333333")
+frame.pack(fill='both', expand='yes')
 
 # Global variables
 isLaunched = False
@@ -14,27 +17,36 @@ verifyOkToLaunch = False
 
 # Function to show abort message box
 def abortMessageCallBack():
-    abortResponse = messagebox.askyesno("Abort Mission?", "Do you really want to abort the mission?")
-    if abortResponse:
+    abort_response = messagebox.askyesno("Abort Mission?", "Do you really want to abort the mission?")
+    if abort_response:
         hasAborted = True
+        verifyOkToLaunch = False
         abortButton.config(state=DISABLED)
 
 
 # Function to show verify message box
 def verifyMessageCallBack():
-    verifyResponse = messagebox.askyesno("Verify Launch", "Do you want to verify for launch?")
-    if verifyResponse:
+    verify_response = messagebox.askyesno("Verify Launch", "Do you want to verify for launch?")
+    if verify_response:
         verifyOkToLaunch = True
         abortButton.config(state=NORMAL)
 
 
+# Abort Mission Label
+abortLabel = Label(top, text="Abort Mission:", bg="#333333", fg="#ffffff")
+abortLabel.place(x=10, y=350)
+
+# Verify Launch Label
+verifyLabel = Label(top, text="Verify Launch:", bg="#333333", fg="#ffffff")
+verifyLabel.place(x=10, y=400)
+
 # Abort Mission Button
 abortButton = Button(top, text="ABORT MISSION", state=DISABLED, bg="red", command=abortMessageCallBack)
-abortButton.place(x=50, y=350)
+abortButton.place(x=100, y=350)
 
 # Verify Launch Button
-verifyButton = Button(top, text="Verify Launch", bg="green", command=verifyMessageCallBack)
-verifyButton.place(x=50, y=400)
+verifyButton = Button(top, text="VERIFY LAUNCH", bg="green", command=verifyMessageCallBack)
+verifyButton.place(x=100, y=400)
 
 # Start window
 top.mainloop()
