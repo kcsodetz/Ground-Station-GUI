76da199b2d00e5f69c76ab5a01582b68f9d6b66c
Ken Sodetz <kensodetz@gmail.com>
2017-10-25 19:28:12 -0400

    comments for readability

--- MainWindow.py
+++ MainWindow.py
@@ -4,21 +4,27 @@ from tkinter import messagebox
 # Set window options
 top = Tk()
 top.geometry("500x500")
-top.title("Rocket GUI")
+top.title("Rocket GUI V 0.1")
 frame = Frame(top, bg="#333333")
 frame.pack(fill='both', expand='yes')
 
+# Initialize and place subFrameLeft
 subFrameLeft = Frame(top, bg="#3C3F41", height="300", width="245", relief=RAISED)
 subFrameLeft.place(x=0, y=0)
 
+# Initialize and place subFrameRight
 subFrameRight = Frame(top, bg="#3C3F41", height="300", width="245", relief=RAISED)
 subFrameRight.place(x=255, y=0)
 
-# Global variables
-isLaunched = False
-hasAborted = False
-verifyOkToLaunch = False
-bgColor = "#333333"
+# ============================ #
+# ===== GLOBAL VARIABLES ===== #
+# ============================ #
+
+isLaunched = False  # has the rocket launched?
+hasAborted = False  # has the process been aborted?
+verifyOkToLaunch = False  # is the system verified for launch?
+bgColor = "#333333"  # background color
+subFrameColor = "#3C3F41"  # sub frame background color
 
 
 # Function to show abort message box
@@ -61,6 +67,14 @@ statusLabel.place(x=300, y=375)
 statusTextLabel = Label(top, text="Current Status:", fg="white", bg=bgColor)
 statusTextLabel.place(x=330, y=350)
 
+# SubFrameLeft Label: Environmental Data
+frameLeftLabel = Label(subFrameLeft, text="Environmental Data:", fg="white", bg=subFrameColor)
+frameLeftLabel.place(x=70, y=5)
+
+# SubFrameLeft Label: System Data
+frameRightLabel = Label(subFrameRight, text="System Data:", fg="white", bg=subFrameColor)
+frameRightLabel.place(x=80, y=5)
+
 
 # Function to change status label
 def statusLabelChange(change_to):
@@ -78,7 +92,8 @@ abortButton = Button(top, text="ABORT MISSION", state=DISABLED, bg="red", comman
 abortButton.place(x=100, y=350)
 
 # Verify Launch Button
-verifyButton = Button(top, text="VERIFY LAUNCH", bg="green", command=verifyMessageCallBack, cursor="shuttle", width="20")
+verifyButton = Button(top, text="VERIFY LAUNCH", bg="green", command=verifyMessageCallBack, cursor="shuttle",
+                      width="20")
 verifyButton.place(x=100, y=400)
 
 # Start window
