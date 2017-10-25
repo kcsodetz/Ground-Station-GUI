eccb313ddcf8417b50ab09d555cacc9bea1c199a
Ken Sodetz <kensodetz@gmail.com>
2017-10-25 17:17:00 -0400

    add enable and disable function for abortMission button, add global status variables

--- MainWindow.py
+++ MainWindow.py
@@ -2,26 +2,39 @@ from tkinter import *
 
 from tkinter import messagebox
 
+# Set window size
 top = Tk()
 top.geometry("500x500")
+
+# Global variables
+isLaunched = False
+hasAborted = False
+verifyOkToLaunch = False
 
 
 # Function to show abort message box
 def abortMessageCallBack():
-    msg = messagebox.askyesno("Abort Mission?", "Do you really want to abort the mission?")
+    abortResponse = messagebox.askyesno("Abort Mission?", "Do you really want to abort the mission?")
+    if abortResponse:
+        hasAborted = True
+        abortButton.config(state=DISABLED)
 
 
 # Function to show verify message box
 def verifyMessageCallBack():
-    msg = messagebox.askyesno("Verify Launch", "Do you want to verify for launch?")
+    verifyResponse = messagebox.askyesno("Verify Launch", "Do you want to verify for launch?")
+    if verifyResponse:
+        verifyOkToLaunch = True
+        abortButton.config(state=NORMAL)
 
 
 # Abort Mission Button
-abortButton = Button(top, text="ABORT MISSION", bg="red", command=abortMessageCallBack)
+abortButton = Button(top, text="ABORT MISSION", state=DISABLED, bg="red", command=abortMessageCallBack)
 abortButton.place(x=50, y=350)
 
 # Verify Launch Button
 verifyButton = Button(top, text="Verify Launch", bg="green", command=verifyMessageCallBack)
 verifyButton.place(x=50, y=400)
 
+# Start window
 top.mainloop()
