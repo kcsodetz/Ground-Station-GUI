0f6bba870516c797d3282880e5d5f728c5a95934
Ken Sodetz <kensodetz@gmail.com>
2017-11-01 18:07:58 -0400

    add bottom frame

--- MainWindow.py
+++ MainWindow.py
@@ -3,18 +3,22 @@ from tkinter import messagebox
 
 # Set window options
 top = Tk()
-top.geometry("500x500")
+top.geometry("600x600")
+hxw = 600 # height and width of top frame
 top.title("Rocket GUI V 0.1")
-frame = Frame(top, bg="#333333")
+frame = Frame(top, width=hxw, height=hxw, bg="#333333")
 frame.pack(fill='both', expand='yes')
 
 # Initialize and place subFrameLeft
-subFrameLeft = Frame(top, bg="#3C3F41", height="300", width="245", relief=RAISED)
+subFrameLeft = Frame(top, bg="#3C3F41", height="300", width=hxw/2 - 5, relief=RAISED)
 subFrameLeft.place(x=0, y=0)
 
 # Initialize and place subFrameRight
-subFrameRight = Frame(top, bg="#3C3F41", height="300", width="245", relief=RAISED)
-subFrameRight.place(x=255, y=0)
+subFrameRight = Frame(top, bg="#3C3F41", height="300", width=hxw/2 - 5, relief=RAISED)
+subFrameRight.place(x=hxw/2 + 5, y=0)
+
+subFrameBottom = Frame(top, bg="#3C3F41", height="200", width=hxw, relief=RAISED)
+subFrameBottom.place(x=0, y=hxw-270)
 
 # ============================ #
 # ===== GLOBAL VARIABLES ===== #
@@ -52,28 +56,28 @@ def verifyMessageCallBack():
 
 
 # Abort Mission Label
-abortLabel = Label(top, text="Abort Mission:", bg=bgColor, fg="white")
-abortLabel.place(x=10, y=350)
+abortLabel = Label(subFrameBottom, text="Abort Mission:", bg=bgColor, fg="white")
+abortLabel.place(x=10, y=55)
 
 # Verify Launch Label
-verifyLabel = Label(top, text="Verify Launch:", bg=bgColor, fg="white")
-verifyLabel.place(x=10, y=400)
+verifyLabel = Label(subFrameBottom, text="Verify Launch:", bg=bgColor, fg="white")
+verifyLabel.place(x=10, y=125)
 
-# Status Label
-statusLabel = Label(top, text="NOT VERIFIED", fg="orange", bg="#808080", width="20", height="2")
-statusLabel.place(x=300, y=375)
+# Status Label to show real time status
+statusLabel = Label(subFrameBottom, text="NOT VERIFIED", fg="orange", bg="#808080", width="20", height="2")
+statusLabel.place(x=hxw/2 + hxw/8, y=80)
 
 # Label to mark status
-statusTextLabel = Label(top, text="Current Status:", fg="white", bg=bgColor)
-statusTextLabel.place(x=330, y=350)
+statusTextLabel = Label(subFrameBottom, text="Current Status:", fg="white", bg=bgColor)
+statusTextLabel.place(x=hxw/2 + hxw/8 + 30, y=50)
 
 # SubFrameLeft Label: Environmental Data
 frameLeftLabel = Label(subFrameLeft, text="Environmental Data:", fg="white", bg=subFrameColor)
-frameLeftLabel.place(x=70, y=5)
+frameLeftLabel.place(x=(hxw/2)/4 + 15, y=5)
 
 # SubFrameLeft Label: System Data
 frameRightLabel = Label(subFrameRight, text="System Data:", fg="white", bg=subFrameColor)
-frameRightLabel.place(x=80, y=5)
+frameRightLabel.place(x=(hxw/2)/4 + 20, y=5)
 
 
 # Function to change status label
@@ -88,13 +92,13 @@ def statusLabelChange(change_to):
 
 
 # Abort Mission Button
-abortButton = Button(top, text="ABORT MISSION", state=DISABLED, bg="red", command=abortMessageCallBack, width="20")
-abortButton.place(x=100, y=350)
+abortButton = Button(subFrameBottom, text="ABORT MISSION", state=DISABLED, bg="red", command=abortMessageCallBack, width="20")
+abortButton.place(x=100, y=55)
 
 # Verify Launch Button
-verifyButton = Button(top, text="VERIFY LAUNCH", bg="green", command=verifyMessageCallBack, cursor="shuttle",
+verifyButton = Button(subFrameBottom, text="VERIFY LAUNCH", bg="green", command=verifyMessageCallBack, cursor="shuttle",
                       width="20")
-verifyButton.place(x=100, y=400)
+verifyButton.place(x=100, y=125)
 
 # Start window
 top.mainloop()
